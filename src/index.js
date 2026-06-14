ee0cfd20e9c949ec1d64c8d991495201e4031d8c
BlaD3r035 <estepaestupinansamuelalejandro@gmail.com>
2026-06-13 23:35:59 -0500

    minor change

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.use(json)
+app.use(express.json());
 
-app.get("/", (req, res) =>{
-  res.status(200).json({"mensaje": "Ruta principal"})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ mensaje: "Ruta principal" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
