c05c8c90b1f1b8456f4ca1572fefe95c993de504
Ivan Soto <ivansoto.marchena@gmail.com>
2026-06-13 23:31:32 -0500

    minor_change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(json)
+
+app.get("/", (req, res) =>{
+  res.status(200).json({"mensaje": "Ruta principal"})
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
